ace81fd0d7b7a9fabc7292e9e1661483515d9c43
arodr <arodr@DESKTOP-KALLIGB>
2020-10-04 21:25:53 -0700

    Added new functionalities to methods.

--- cse360assignment1/src/cse360assignment02/AddingMachine.java
+++ cse360assignment1/src/cse360assignment02/AddingMachine.java
@@ -1,14 +1,16 @@
 /**
  * The following program, AddingMachine, utilizes methods that adds, subtracts, gets the total,
- * prints Strings, and clears memory
+ * prints Strings, and clears memory of integers.
  * 
  * @author Angel Rodriguez
  */
 
 package cse360assignment02;
 
-public class AddingMachine {
+public class AddingMachine 
+{
 	private int total;
+	private String intKeeper;
 	  
 	/**
 	* Constructor of the class.
@@ -16,7 +18,8 @@ public class AddingMachine {
 	* @return no return type
 	*/
 	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
+	    total = 0;  // initialize to 0.
+	    intKeeper = "0"; // initialize to "0".
 	  }
 	  
 	/**
@@ -25,35 +28,49 @@ public class AddingMachine {
 	* @return returns an int that represents total
 	*/
 	  public int getTotal () {
-	    return 0;
+	    return total; // returns total
 	  }
+	  
 	/**
 	* Calculates sum value
 	* @param value (int)
 	* @return no return type
 	*/
-	  public void add (int value) {
+	  public void add (int value) 
+	  {
+		  total += value; // adds value to total.
+		  intKeeper += " + " + value; // appends operator "+" and value.
 	  }
+	  
 	/**
 	* Calculates difference value
 	* @param value (int)
 	* @return no return type
 	*/
-	  public void subtract (int value) {
+	  public void subtract (int value) 
+	  {
+		  total = total - value; // subtracts total from value.
+		  intKeeper += " - " + value; // appends operator "-" and value.
 	  }
+	  
 	/**
-	* Prints string
+	* Prints transaction history in the form of a string.
 	* @param no parameters
 	* @return returns String
 	*/
-	  public String toString () {
-	    return "";
+	  public String toString () 
+	  {
+	    return intKeeper; // returns string of history operations.
 	  }
+	  
 	/**
 	* Clears data
 	* @param no parameters
 	* @return no return type
 	*/
-	  public void clear() {
+	  public void clear() 
+	  {
+		  intKeeper = "0"; //set string to "0".
+		  total = 0; // reset total to 0.
 	  }
 }
