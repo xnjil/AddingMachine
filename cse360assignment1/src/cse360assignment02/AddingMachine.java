69b1f3c01e8d5844620030067a09787c78207d81
arodr <arodr@DESKTOP-KALLIGB>
2020-10-04 19:34:39 -0700

    Initial commit of project.

---
+++ cse360assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,59 @@
+/**
+ * The following program, AddingMachine, utilizes methods that adds, subtracts, gets the total,
+ * prints Strings, and clears memory
+ * 
+ * @author Angel Rodriguez
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	  
+	/**
+	* Constructor of the class.
+	* @param no parameters
+	* @return no return type
+	*/
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	/**
+	* Gets total value
+	* @param no parameters
+	* @return returns an int that represents total
+	*/
+	  public int getTotal () {
+	    return 0;
+	  }
+	/**
+	* Calculates sum value
+	* @param value (int)
+	* @return no return type
+	*/
+	  public void add (int value) {
+	  }
+	/**
+	* Calculates difference value
+	* @param value (int)
+	* @return no return type
+	*/
+	  public void subtract (int value) {
+	  }
+	/**
+	* Prints string
+	* @param no parameters
+	* @return returns String
+	*/
+	  public String toString () {
+	    return "";
+	  }
+	/**
+	* Clears data
+	* @param no parameters
+	* @return no return type
+	*/
+	  public void clear() {
+	  }
+}
